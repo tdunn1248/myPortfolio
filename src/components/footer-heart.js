fb8cafbbd6c985eb501bbc58d61807c964873d73
tdunn1248 <tdunn1248@gmail.com>
2017-11-22 09:01:45 -0800

    bring in heart

--- src/components/footer-heart.js
+++ src/components/footer-heart.js
@@ -4,7 +4,7 @@ export default class FooterHeart extends React.Component {
   render() {
     return (
       <div className='footer-heart'>
-        <span className=''>TD</span>
+        <span className='glyphicon glyphicon-heart icon-white'></span>
       </div>
     )
   }
