e3f0edf9a1832771e582c4fa2757e73490cb145e
tdunn1248 <tdunn1248@gmail.com>
2018-04-12 00:33:13 -0700

    remove homepage header

--- src/components/homepage.js
+++ src/components/homepage.js
@@ -6,13 +6,7 @@ export default class HomePageBackground extends Component {
     return (
       <div className='homepage-background'>
         <div className='homepage-welcome'>
-          <div className='welcome-container'>
-            <h1>Full Stack Software Developer</h1>
-            <h6>
-              Based out of the San Francisco Bay Area,
-              I enjoy working across the stack to bring ideas and useful services to life.
-            </h6>
-          </div>
+          
         </div>
       </div>
     )
